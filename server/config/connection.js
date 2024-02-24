a30dbbcb6a725a3fe4a654398ded0bd38c7151be
chilejay7 <codyburk7@yahoo.com>
2024-02-23 23:59:35 -0700

    db config added

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,3 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose
     .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/yelp_camp')
+    .then(() => {
+        console.log('The database is up and running')
+    })
+    .catch((err) => {
+        console.error(err);
+    })
