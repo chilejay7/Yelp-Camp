a99c48324c2e887f8572fd203e1d586f29109861
chilejay7 <codyburk7@yahoo.com>
2024-02-24 11:24:19 -0700

    adds morgan logging middleware to server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
+const morgan = require('morgan');
 
 const PORT = 7075;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.send('Test route and server are responding.')
@@ -14,6 +16,6 @@ app.get('/', (req, res) => {
 
 db.once('open', () => {
     app.listen(PORT, () => {
-        console.log('This server is up and listening on PORT', PORT);
+        console.log('The server is up and listening on PORT', PORT);
     })
 });
