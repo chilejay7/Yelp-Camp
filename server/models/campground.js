d80d552013faaf1466c3b7139e9538e59043b61f
chilejay7 <codyburk7@yahoo.com>
2024-03-03 17:53:14 -0700

    Campground model updated

--- server/models/campground.js
+++ server/models/campground.js
@@ -1,11 +1,23 @@
-const { Schema, model } =  require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const campgroundSchema = new Schema({
-    title: String,
-    price: String,
-    description: String,
-    location: String
-});
+const campgroundSchema = new Schema(
+    {
+        title: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        price: {
+            type: String,
+        },
+        description: {
+            type: String,
+        },
+        location: {
+            type: String,
+        },
+    }
+);
 
 const Campground = model('Campground', campgroundSchema);
 
