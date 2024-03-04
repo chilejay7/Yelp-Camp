be3352500b982712be60329da0a9a8e6ea6df9dd
chilejay7 <codyburk7@yahoo.com>
2024-03-03 17:35:19 -0700

    variable for db name added to connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const db = 'yelp_camp';
 
 mongoose
-    .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/yelp_camp')
+    .connect(process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/${db}`)
     .then(() => {
         console.log('The database is up and running')
     })
