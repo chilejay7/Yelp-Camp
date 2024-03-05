e55beea3dcca7e0fdd7dc33f8388c9d7b628788f
chilejay7 <codyburk7@yahoo.com>
2024-03-04 20:15:26 -0700

    react build loading without errors

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 7075,
+    open: true,
+    proxy: {
+      '/campgrounds': {
+        target: 'http://localhost:7075',
+        secure: false,
+        changeOrigin: true
+      }
+    }
+  }
 })
