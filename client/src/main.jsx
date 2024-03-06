3b87b0e86d0b01d1f17fbd3d6064db688dda38f0
chilejay7 <codyburk7@yahoo.com>
2024-03-05 22:03:09 -0700

    main.jsx updated to include router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='error-header'>Page does not exist.  Please try another route.</h1>,
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <RouterProvider router={router} />
 )
--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 7075,
+    port: 3000,
     open: true,
     proxy: {
       '/campgrounds': {
