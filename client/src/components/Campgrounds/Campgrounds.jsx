95483aed1b2d1fa134ce31163529db99a3a472bb
chilejay7 <codyburk7@yahoo.com>
2024-03-07 22:50:00 -0700

    useEffect updated on campgrounds.jsx

--- client/src/components/Campgrounds/Campgrounds.jsx
+++ client/src/components/Campgrounds/Campgrounds.jsx
@@ -11,26 +11,29 @@ export default function Campgrounds() {
         const response = await getCampgrounds();
         const data = await response.json();
         setCampgrounds(data);
-        
-    console.log('Campgrounds:', campgrounds)
+        console.log('Campgrounds:', campgrounds)
     }
 
-    // useEffect(async () => {
-    //     const data = await getCampgrounds();
-    //     console.log(data);
-    //     setCampgrounds(data);
-    // }, []);
+    useEffect(() => {
+        const initialData = async () => {
+            const response = await getCampgrounds();
+            const data = await response.json();
+            setCampgrounds(data);
+            console.log('Campgrounds:', campgrounds)
+        }
+        initialData();
+    }, []);
 
 
     return (
         <>
             <h2>Campgrounds</h2>
 
-            <Button onClick={handleSearch}>Search Campgrounds</Button>
+            <Button variant="contained" onClick={handleSearch}>Find Campgrounds</Button>
 
             <ul>
                 {campgrounds.map(camp =>
-                    <li key={camp.id}>{camp.title}</li>
+                    <li key={camp._id}>{camp.title}</li>
                 )}
             </ul>
         </>
