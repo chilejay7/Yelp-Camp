b275cd323a22aefe61dfd814f6a2597316f25d93
chilejay7 <codyburk7@yahoo.com>
2024-03-08 18:00:38 -0700

    footer component created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 export default function App() {
   return (
     <>
-    <Navbar />
-    <Outlet />
-  </>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
   )
 }
