24025e461a1d90e8a1ed15b0a7ce0afb3dcbab10
chilejay7 <codyburk7@yahoo.com>
2024-03-08 17:48:47 -0700

    CampgroundByID.jsx added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Campgrounds from './components/Campgrounds/Campgrounds.jsx'
+import CampgroundById from './components/CampgroundByID/CamgroundByID.jsx'
 import './index.css'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <h1 className='error-header'>Page does not exist.  Please try another route.</h1>,
+    errorElement: <h2 className='error-header'>Page does not exist.  Please try another route.</h2>,
     children: [
       {
         index: true,
@@ -19,6 +21,10 @@ const router = createBrowserRouter([
       {
         path: '/campgrounds',
         element: <Campgrounds />
+      },
+      {
+        path: '/campgrounds/:id',
+        element: <CampgroundById />
       }
     ]
   }
