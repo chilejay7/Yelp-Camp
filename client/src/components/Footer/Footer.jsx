017b7f17d9003dc8201e99d05a89e123e3e6d337
chilejay7 <codyburk7@yahoo.com>
2024-03-11 06:22:58 -0600

    footer updated

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import './Footer.css';
 export default function Footer() {
     return (
         <footer id="footer">
-            <h4>Footer</h4>
+            <h4>Cody Burkholder</h4>
+            <p>Copyright 2024</p>
         </footer>
     )
 }
