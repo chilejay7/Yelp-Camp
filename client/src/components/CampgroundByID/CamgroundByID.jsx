d49a4024ca75847f6e517cd4c935e088e6fc1897
chilejay7 <codyburk7@yahoo.com>
2024-03-11 20:12:34 -0600

    try catch added to campground by id component

--- client/src/components/CampgroundByID/CamgroundByID.jsx
+++ client/src/components/CampgroundByID/CamgroundByID.jsx
@@ -4,25 +4,34 @@ import { useState, useEffect } from 'react';
 
 const location = window.location.href.split('/')
 const campID = location[4];
+console.log('The campID is:', campID);
 
 export default function CampgroundById() {
 
     const [campground, setCampground] = useState({});
 
     const findCamp = async () => {
-        const response = await getCampgroundID(campID);
-        const campData = await response.json();
-        setCampground(campData);
+        try {
+            const response = await getCampgroundID(campID);
+            const campData = await response.json();
+            setCampground(campData);
+        } catch (err) {
+            console.error('Unable to fetch campground:', err)
+        }
+        
     };
 
     useEffect(() => {
         findCamp();
-    }, [])
+    }, []);
+
+    console.log('The campground in state is:', campground);
 
     return (
         <>
             <h2>Campground ID!</h2>
             <p>Here is your campground...</p>
+            <p>{campground.title} - {campground.location}</p>
         </>
     )
 }
