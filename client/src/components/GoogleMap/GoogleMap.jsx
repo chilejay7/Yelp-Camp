55440512eb50185a778836d69f7c989b9e42e39a
chilejay7 <codyburk7@yahoo.com>
2024-05-07 21:07:48 -0600

    custom marker image added to map

--- client/src/components/GoogleMap/GoogleMap.jsx
+++ client/src/components/GoogleMap/GoogleMap.jsx
@@ -11,12 +11,15 @@ export default function GoogleMap({ title, latitude, longitude, location = "Wood
     const position = { lat: 39.6623872, lng: -104.93952 };
     const apiKey = import.meta.env.VITE_MAP_KEY;
     const mapId = import.meta.env.VITE_MAP_IDNUM;
+    const markerImg = "./images/camping-tent.png";
 
     return (
         <APIProvider apiKey={apiKey}>
             <div id="google-map">
                 <Map zoom={9} center={position} style={{ height: "50vh" }} mapId={mapId}></Map>
-                <AdvancedMarker position={position} ></AdvancedMarker>
+                <AdvancedMarker position={position} >
+                    <img src={markerImg} width={32} height={32}></img>
+                </AdvancedMarker>
             </div>
         </APIProvider>
     );
